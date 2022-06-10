f9001f25b2cdd75921a32b55079ce867010b27d6
magdielmarques <contato.magdiel@gmail.com>
2022-06-10 19:42:35 -0400

    Adding promise  all

--- class-n04/index.js
+++ class-n04/index.js
@@ -7,10 +7,13 @@ const main = async () => {
   
   console.time('TESTE DE TEMPO DE EXECUCAO')
   const usuario = await obterUsuario();
-  const telefone = await obterTelefone(usuario.id);
-  const endereco = await obterEndereco(usuario.id);
+  // const telefone = await obterTelefone(usuario.id);
+  // const endereco = await obterEndereco(usuario.id);
+
+  const result = await Promise.all([obterTelefone(usuario.id), obterEndereco(usuario.id)])
   console.timeEnd('TESTE DE TEMPO DE EXECUCAO')
   
-  console.log(usuario, endereco, telefone);
+  // console.log(usuario, endereco, telefone);
+  console.log(usuario, result[0], result[1]);
 }
 main()
