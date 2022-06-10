264a0bfb19c3c39636fff1a5054ad4e882c94287
magdielmarques <contato.magdiel@gmail.com>
2022-06-10 19:14:10 -0400

    Finishing brief study of promise

--- class-n03/index.js
+++ class-n03/index.js
@@ -1,9 +1,20 @@
 // Promise
+// obs.: utilizado apenas para fins de estudo de codigos aninhados
 
 import { obterUsuario } from './obterUsuario.js';
 import { obterTelefone } from './obterTelefone.js';
 import { obterEndereco } from './obterEndereco.js';
 
-obterUsuario().then((result) => console.log('usuario', result))
-obterEndereco(1).then((result) => console.log('endereco', result))
-obterTelefone(1).then((result) => console.log('telefone', result))
+obterUsuario()
+  .then((usuario) => 
+    obterEndereco(usuario.id)
+      .then((endereco) => {
+          return { endereco: endereco, usuario: usuario }
+      })
+  )
+  .then((obj) => 
+      obterTelefone(obj.usuario.id)
+        .then((result) => console.log(result, obj))
+  )
+  .catch((err) => console.log(err))
+      
