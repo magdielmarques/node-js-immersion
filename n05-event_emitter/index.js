b57a060bafd536b1220e85291cd4d7576c3767ac
magdielmarques <contato.magdiel@gmail.com>
2022-06-13 19:00:51 -0400

    First emitter

--- n05-event_emitter/index.js
+++ n05-event_emitter/index.js
@@ -1,1 +1,13 @@
-// 29 s
+const EventEmitter = require('events')
+class MyEmitter extends EventEmitter {
+  // extends class atribute  
+}
+
+const myEmitter = new MyEmitter()
+const nameOfEvent = 'user:click'
+
+myEmitter.on(nameOfEvent, (click) => {
+  console.log('Some new click', click)
+})
+
+myEmitter.emit(nameOfEvent, 'in new buttom')
