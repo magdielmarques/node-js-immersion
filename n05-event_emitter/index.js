db756446ad70de5e7a0d7ba53d6befa1bb8cafb4
magdielmarques <contato.magdiel@gmail.com>
2022-06-13 19:18:53 -0400

    Finlizing brief study of event emitter

--- n05-event_emitter/index.js
+++ n05-event_emitter/index.js
@@ -10,8 +10,32 @@ myEmitter.on(nameOfEvent, (click) => {
   console.log('Some new click', click)
 })
 
-// myEmitter.emit(nameOfEvent, 'in new buttom')
+/*
+myEmitter.emit(nameOfEvent, 'in new buttom')
 let count = 0;
 setInterval(() => {
   myEmitter.emit(nameOfEvent, `in new buttom blue number ${count++}.`)
 }, 1000)
+*/
+
+const stdin = process.openStdin()
+
+stdin.addListener('data', (value) => {
+  console.log(`You're typping ${value.toString().trim()}`);
+})
+
+/* 
+
+// some common problem below
+
+const main = () => {
+  return new Promise((resolve, reject) => {
+    stdin.addListener('data', (value) => {
+      return resolve(value)
+    })
+  })
+}
+
+main().then((result) => console.log('You are typping', result.toString().trim()));
+
+*/
