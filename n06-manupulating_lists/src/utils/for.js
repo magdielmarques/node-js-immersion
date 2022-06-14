87d6c980340df5e061d50996edc283b3c03183ce
magdielmarques <contato.magdiel@gmail.com>
2022-06-14 18:52:50 -0400

    Testing performance of higher order functions FOR

--- n06-manupulating_lists/src/utils/for.js
+++ n06-manupulating_lists/src/utils/for.js
@@ -2,8 +2,18 @@ const service = require('../data/service.js')
 
 const main = async () => {
   try {
-    const result = await service.getPeople('r2')
-    console.log(result)
+    const result = await service.getPeople('a')
+
+    const names = []
+
+    console.time('tempo de execucao no FOR')
+    for (let i = 0; i <= result.results.length - 1; i++) {
+      const eachResult = result.results[i];
+      names.push(eachResult.name)
+    }
+    console.timeEnd('tempo de execucao no FOR')
+
+    console.log('names -> ', names)
   } catch (error) {
     console.error('Algo nao saio como espera.', error)
   } finally {
