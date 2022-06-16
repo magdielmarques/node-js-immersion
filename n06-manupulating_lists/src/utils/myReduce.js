364008bff867e234a7c235a22cd6002595ede456
magdielmarques <contato.magdiel@gmail.com>
2022-06-15 20:39:48 -0400

    Testing performance of my own REDUCE FUNCTION

---
+++ n06-manupulating_lists/src/utils/myReduce.js
@@ -0,0 +1,37 @@
+const { getPeople } = require('../data/service.js')
+
+Array.prototype.myReduce = function(callback) {
+  let result = 0;
+  for (let index = 0; index < this.length; index++) {
+    if (index === 0) continue;
+    returnedValue = callback(this[index - 1], this[index])
+    result += returnedValue
+  }
+
+  return result
+}
+
+const main = async () => {
+  try {
+    const { results } = await getPeople('a')
+    // console.log('results - ', results);
+
+    console.time('tempo de execucao no MY REDUCE')
+    
+    const height = results.map(item => parseInt(item.height))
+    console.log('height - ', height);
+
+    const addedHeight = height.myReduce((oldValue, nextValue) => oldValue + nextValue)
+    console.log('addedHeight - ', addedHeight);
+    
+    console.timeEnd('tempo de execucao no MY REDUCE')
+  } catch (error) {
+    console.error('Algo nao saio como espera.', error)
+  } finally {
+    console.log('Finalizando execucao!')
+  }
+}
+
+module.exports = {
+  main
+}
