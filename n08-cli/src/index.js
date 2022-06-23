4044988ff330706c5982e26c8dde029d9544f871
magdielmarques <contato.magdiel@gmail.com>
2022-06-22 20:33:39 -0400

    Log in commander

--- n08-cli/src/index.js
+++ n08-cli/src/index.js
@@ -4,15 +4,15 @@ const service = require('./database/index');
 const main = async () => {
   Commander
     .version('v1 project by Magdiel') // set version og my project
-      .option('-n, --name [value]', "Name of eletric guitar") // option to insert value
-      .option('-l, --color [value', "Name of color") // option to insert value
+      .option('-N, --name [value]', "Name of eletric guitar") // option to insert value
+      .option('-L, --color [value', "Name of color") // option to insert value
 
-      .option('-c, --create', "Create an new eletric guitar") // option of CRUD
+      .option('-C, --create', "Create an new eletric guitar") // option of CRUD
 
     .parse(process.argv) // get everything typed of user
-
+    
     try {
-      if(Commander.c) {
+      if(Commander.createOption) {
         console.log(Commander);
         // const result = await service.create()
       }
