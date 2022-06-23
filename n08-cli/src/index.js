03d08095c532144c61dcd78ddfac9474f0e73e98
magdielmarques <contato.magdiel@gmail.com>
2022-06-22 20:27:49 -0400

    Feat: new options

--- n08-cli/src/index.js
+++ n08-cli/src/index.js
@@ -1,10 +1,24 @@
 const Commander = require('commander');
+const service = require('./database/index');
 
 const main = async () => {
   Commander
-    .version('v1 do Magdiel')
+    .version('v1 project by Magdiel') // set version og my project
+      .option('-n, --name [value]', "Name of eletric guitar") // option to insert value
+      .option('-l, --color [value', "Name of color") // option to insert value
+
+      .option('-c, --create', "Create an new eletric guitar") // option of CRUD
 
     .parse(process.argv) // get everything typed of user
+
+    try {
+      if(Commander.c) {
+        console.log(Commander);
+        // const result = await service.create()
+      }
+    } catch (error) {
+      throw new Error(error)
+    }
 }
 
 main()
