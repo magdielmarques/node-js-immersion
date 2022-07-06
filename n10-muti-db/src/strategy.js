b084da0bafe3d471674eda04a51298f558ee1dfb
magdielmarques <contato.magdiel@gmail.com>
2022-07-05 22:28:56 -0400

    Fake interface

--- n10-muti-db/src/strategy.js
+++ n10-muti-db/src/strategy.js
@@ -1,5 +1,25 @@
-class NotImplementedException extends Erros {
+class NotImplementedException extends Erro {
   constructor() {
     super('New message added with extends class: Not Implemented Exception!')
   }
 } 
+
+
+// fake interface
+class ICrud {
+  create(item) {
+    throw new NotImplementedException()
+  }
+
+  read(query) {
+    throw new NotImplementedException()
+  }
+
+  update(id, item) {
+    throw new NotImplementedException()
+  }
+
+  delete(id) {
+    throw new NotImplementedException()
+  }
+}
