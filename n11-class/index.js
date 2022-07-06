00deff511512df585d18f7b081179f80b7fd0133
magdielmarques <contato.magdiel@gmail.com>
2022-07-05 20:26:50 -0400

    Class examples

---
+++ n11-class/index.js
@@ -0,0 +1,32 @@
+// classic way
+class Person {
+  constructor(name, age) { // atributes
+    this._name = name
+    this._age = age
+  } 
+
+  set name(value) { // can get parameter
+    // this._name = value
+    console.log(`What happens when you try put some value into that atribute ->`, value)
+  }
+
+  set age(value) {
+    console.log(`What happens when you try put some value into that atribute ->`, value)
+  }
+
+  get name() { // can't get parameter
+    return console.log(`You're trying get some value here, so i can do something`)
+  }
+
+  walk() { //methods
+    return console.log(`${this._name} is walking.`);
+  }
+}
+
+const person = new Person()
+// example using set
+// person.name = 'Magdiel'
+// person.age = 24
+
+// example using get
+// person.name
