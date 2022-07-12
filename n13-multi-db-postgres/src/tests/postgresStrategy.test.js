d5c89665654e0e3c722ecf2652081eea6ddfbcab
magdielmarques <contato.magdiel@gmail.com>
2022-07-11 22:29:12 -0400

    create test

--- n13-multi-db-postgres/src/tests/postgresStrategy.test.js
+++ n13-multi-db-postgres/src/tests/postgresStrategy.test.js
@@ -3,11 +3,21 @@ const Postgres = require('../db/strategies/postgres');
 const Context = require('../db/strategies/base/contextStrategy');
 
 const context = new Context(new Postgres())
+const MOCK_GUITAR_CREATE = {
+  brand: 'Taylor',
+  color: 'Black'
+} 
+
 
 describe('Postgres Strategy', function () {
   this.timeout(Infinity) 
   it('PostgresSQL Connection', async function () {
     const result = await context.isConected()
     assert.equal(result, true)
+  })
+  it('Should create', async function () {
+    const result = await context.create(MOCK_GUITAR_CREATE)
+
+    assert.deepEqual(result, MOCK_GUITAR_CREATE)
   })
 })
