9ec671ed74793620b669dc7a4f659108b184fef3
magdielmarques <contato.magdiel@gmail.com>
2022-07-13 21:23:17 -0400

    mongo script

--- n14-multi-db-postgres-mongodb/src/scripts/mongodb.js
+++ n14-multi-db-postgres-mongodb/src/scripts/mongodb.js
@@ -1,20 +1,23 @@
+// cria usuario
+docker exec -it mongodb-estudo mongo --host localhost -u admin -p senhadmin --authenticationDatabase admin --eval "db.getSiblingDB('estudo').createUser({user: 'magdielmarques', pwd: 'minhasenha', roles: [{role: 'readWrite', db: 'estudo'}]})"
+
 // conectar via terminar ao mongo
 docker ps
-docker exec -it d0be7146c4e1 mongo -u magdielmarques -p minhasenha --authenticationDatabase estudo
+// docker exec -it 7c5db563315e mongo -u magdielmarques -p minhasenha --authenticationDatabase estudo
+docker exec -it mongodb-estudo mongo -u magdielmarques -p minhasenha --authenticationDatabase estudo
 
 // show databases
 show dbs
 
 // mudando o contexto para uma database
-use guitars
+use estudo
 
 // mostra as collections (tabelas)
 show collections
 
 // adicionando tabelas à database
-db.info.insert({
-  brand: 'Fender',
-  pricing: 1000,
-  age: 1996,
-  color: 'White'
+db.person.insert({
+  name: 'Magdiel',
+  age: 24,
+  color: 'Black'
 })
