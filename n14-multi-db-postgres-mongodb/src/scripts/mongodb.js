7addb65363fc0bc60f572be4117687c03492be64
magdielmarques <contato.magdiel@gmail.com>
2022-07-13 21:46:21 -0400

    update scripts mongo

--- n14-multi-db-postgres-mongodb/src/scripts/mongodb.js
+++ n14-multi-db-postgres-mongodb/src/scripts/mongodb.js
@@ -27,3 +27,27 @@ db.person.find()
 
 // encontra tabela e exibe bonito
 db.person.find().pretty()
+
+// posso rodar comandos JS dentro do terminal do mongodb
+for (let i=0; i < 20; i++) {
+  db.person.insert({
+    name: `Magdiel ${i}`,
+    age: i,
+    color: 'Black'
+  })
+}
+
+// conta quantidade de objetos inseridos na collection
+db.person.count()
+
+// traz apenas 1
+db.person.findOne()
+
+// traz 5 itens e ordenar pelo nome em ordem do menor para o maior
+db.person.find().limit(5).sort({ name: -1})
+
+// passa o where vazio como parametro; traz apenas a coluna nome e culta o id
+// utiliza o 1 ou 0 para sinalizar o que desejo trazer ou nao (true ou false)
+db.person.find({}, { name: 1, _id:0 })
+
+
