01fc1d7cfd5292d0bef0f148fabccbde397cdd58
magdielmarques <contato.magdiel@gmail.com>
2022-07-14 00:31:56 -0400

    success connection on mongodb with mongoose

--- n14-multi-db-postgres-mongodb/src/mongodbExample.js
+++ n14-multi-db-postgres-mongodb/src/mongodbExample.js
@@ -1,2 +1,15 @@
 // instalar driver
 // npm i mongoose
+// Mongoose.connect('mongodb://user:passwordhost@host:port/database')// conectar com o banco por URI
+
+const mongoose = require('mongoose')
+const URI = 'mongodb://localhost:27017/mongodb-estudo'
+
+mongoose.connect(URI, error => {
+                        if (!error) return;
+                        console.error('Falha na conexão! \n \n', error)
+                })
+
+const connection = mongoose.connection
+connection.once('open', () => console.log('database rodando!'))
+
